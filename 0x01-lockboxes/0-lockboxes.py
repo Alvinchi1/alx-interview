cf871310640d3ee8311bff041c22dfbf79e4698f
Alvinchi1 <alvinchibundo007>
2024-10-07 04:40:42 -0700

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""this script will unlock the list of lists"""
+
+
+def canUnlockAll(boxes):
+
+
+""" this takes list of lists to unlock other lists"""
+    
+    keys =[0]
+    for key in keys:
+        if boxkey not in keys and boxkey < len(boxes):
+            keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
+
